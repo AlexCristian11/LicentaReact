ca0464dcc79482d54913bf8c3ae3cc9499907508
AlexCristian11 <alexpopescu2002@yahoo.com>
2023-06-25 17:17:55 +0300

    Added Category and Product

---
+++ Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LicentaReact.Models
+{
+	public class Product
+	{
+		[Key]
+		public int Id { get; set; }
+
+		public string Nume { get; set; } = null!;
+
+		public string Descriere { get; set; } = null!;
+
+		[Column(TypeName = "decimal(6, 2)")]
+		public decimal Pret { get; set; }
+
+		public int Stoc { get; set; } 
+		public string Imagine { get; set; } = null!;
+
+		public int CategoryId { get; set; }
+		public Category Category { get; set; } = null!;
+
+		public virtual ICollection<CartProduct> CartProducts { get; set; } = new List<CartProduct>();
+	}
+}
